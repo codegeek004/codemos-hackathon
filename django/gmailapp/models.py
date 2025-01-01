c5044f784728910713fc3b45dc470b35635540bf
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2025-01-01 17:51:24 +0530

    model for storing the gmail reference

--- django/gmailapp/models.py
+++ django/gmailapp/models.py
@@ -4,8 +4,8 @@ print('models file initiated')
 class Gmail(models.Model):
     print('gmail models loaded')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    message_id = models.CharField(max_length=1000)
-    thread_id = models.CharField(max_length=1000)
+    message_id = models.CharField(max_length=1000, default="123")
+    thread_id = models.CharField(max_length=1000, default="123")
 
     def __str__(self):
         return self.message_id
