5088924d5ea86e4092770adbab182b4e59e68b4d
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2025-01-09 19:08:03 +0530

    removed exception line

--- django/gmailapp/views.py
+++ django/gmailapp/views.py
@@ -43,7 +43,7 @@ def delete_emails_view(request):
             return redirect('delete_emails')
 
     except Exception as e:
-        print(e)
+#        print(e)
         messages.error(request, "An error occurred while processing your request.")
         return redirect('delete_emails')
 
