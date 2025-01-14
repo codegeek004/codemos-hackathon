6881431b5f05c40b9ab8bfa4b94722722331f0f1
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2025-01-14 19:41:56 +0530

    added url for check_task_status function

--- django/gmailapp/urls.py
+++ django/gmailapp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from . import views  # Import your views from the current app
-
+from . import views  
 urlpatterns = [
     # path('show/', views.show, name='show'),  # Test endpoint to check OAuth completion
     path('delete_emails/', views.delete_emails_view, name='delete_emails'),  # Endpoint to delete emails
     path('recover_emails/', views.recover_emails_from_trash_view, name='recover_emails'),
-    path('polling/<str:task_id>/', views.polling_view, name='polling_view'), 
+    path('check_task_status/<str:task_id>/', views.check_task_status_view, name='check_task_status'),
+
+    # path('polling/<str:task_id>/', views.polling_view, name='polling_view'), 
     # path('fetch_google_token/', views.fetch_google_token, name='fetch_google_token'),  # Endpoint to fetch Google tokens
+    ##webhook url
+    # path('delete_emails/webhook/task_completed/', views.task_completed_webhook, name='task_completed_webhook')
 ]
