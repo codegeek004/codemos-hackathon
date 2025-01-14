d493d6f0afbbf9dc24c286db3b5d238e5acb2338
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2025-01-14 11:25:48 +0530

    added url for polling_view

--- django/gmailapp/urls.py
+++ django/gmailapp/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # path('show/', views.show, name='show'),  # Test endpoint to check OAuth completion
     path('delete_emails/', views.delete_emails_view, name='delete_emails'),  # Endpoint to delete emails
     path('recover_emails/', views.recover_emails_from_trash_view, name='recover_emails'),
-
+    path('polling/<str:task_id>/', views.polling_view, name='polling_view'), 
     # path('fetch_google_token/', views.fetch_google_token, name='fetch_google_token'),  # Endpoint to fetch Google tokens
 ]
