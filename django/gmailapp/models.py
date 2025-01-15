31e3f8c095d2823e7a2677a54a083ae94ce05da8
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2025-01-15 11:52:27 +0530

    added model for fetching the recovery status

--- django/gmailapp/models.py
+++ django/gmailapp/models.py
@@ -6,5 +6,14 @@ class TaskStatus(models.Model):
 	task_id = models.CharField(max_length=200, unique=True)
 	status = models.CharField(max_length=40, default="PENDING")
 	result = models.TextField(null=True, blank=True)
+	deleted_count = models.IntegerField(default=0)
 	created_at = models.DateTimeField(auto_now_add=True)
 	
+
+class RecoverStatus(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	task_id = models.CharField(max_length=200, unique=True)
+	status = models.CharField(max_length=40, default="PENDING")
+	result = models.TextField(null=True, blank=True)
+	recover_count = models.IntegerField(default=0)
+	created_at = models.DateTimeField(auto_now_add=True)
