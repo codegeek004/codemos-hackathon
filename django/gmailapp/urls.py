245314a9df75b8be61e8240174e5e408e1f1b68e
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2025-01-15 11:49:49 +0530

    added url for checking the recovery function

--- django/gmailapp/urls.py
+++ django/gmailapp/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('delete_emails/', views.delete_emails_view, name='delete_emails'),  # Endpoint to delete emails
     path('recover_emails/', views.recover_emails_from_trash_view, name='recover_emails'),
     path('check_task_status/<str:task_id>/', views.check_task_status_view, name='check_task_status'),
+    path('recovery_task_status/<str:task_id>/', views.email_recovery_status_view, name='email_recovery_status'),
 
     # path('polling/<str:task_id>/', views.polling_view, name='polling_view'), 
     # path('fetch_google_token/', views.fetch_google_token, name='fetch_google_token'),  # Endpoint to fetch Google tokens
