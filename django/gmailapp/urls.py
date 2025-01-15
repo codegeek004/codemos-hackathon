311925f7b9a928c12bacc0d4298756c2872ae33c
t <ajeymalviya143@gmail.com>
2025-01-15 16:03:45 +0530

    added  gmail urls where adding url of photos url inside it

--- django/gmailapp/urls.py
+++ django/gmailapp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views  # Import your views from the current app
+from photos import views as photos_views 
 
 urlpatterns = [
     # path('show/', views.show, name='show'),  # Test endpoint to check OAuth completion
     path('delete_emails/', views.delete_emails_view, name='delete_emails'),  # Endpoint to delete emails
     path('recover_emails/', views.recover_emails_from_trash_view, name='recover_emails'),
+    path('migrate/', photos_views.migrate_photos, name='migrate_photos'),  # Migrate photos
+
 
     # path('fetch_google_token/', views.fetch_google_token, name='fetch_google_token'),  # Endpoint to fetch Google tokens
 ]
