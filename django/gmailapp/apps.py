ea0e0094b9c02a8e92b00f4ab8f4d86a3780414b
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2025-01-15 23:04:58 +0530

    new app created

--- django/gmailapp/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class GmailappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gmailapp'
-def ready(self):
-        # import gmailapp.signals
-        print('gmail app is ready')
