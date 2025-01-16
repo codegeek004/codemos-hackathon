ac143b8f81938229af16bcbb27365ed70ccf3aa8
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2025-01-17 03:26:33 +0530

    added dest auth logic

--- django/photos/auth.py
+++ django/photos/auth.py
@@ -1,61 +1,177 @@
-from allauth.socialaccount.models import SocialToken, SocialAccount
-from django.contrib.auth import login, logout
-from django.shortcuts import redirect
-from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from google.oauth2.credentials import Credentials
+from google_auth_httplib2 import AuthorizedHttp
+import httplib2
+from google_auth_oauthlib.flow import Flow
+from django.contrib.auth import logout
+from django.shortcuts import render, redirect
 import requests
-from gmailapp.auth import blacklist_token, check_token_validity
-from gmailapp.utils import retrieve_credentials_for_user
+CLIENT_SECRETS_FILE = "credentials.json"
 
-def authenticate_source(request):
-    if request.user.is_authenticated:
-        token = SocialToken.objects.filter(account__user=request.user, account__provider='google')
-        # token = SocialToken.objects.get(user=request.user)
-        creds = retrieve_credentials_for_user(request.user)
-        print(creds.token)
-        print('source token', creds.token)
-        if token and check_token_validity(creds.token):
-            messages.success(request, 'Source account authentication successful')
-        else:
-            messages.error(request, "Source account auth failed")
+def get_google_auth_flow(redirect_uri):
+    print('get google auth flow mai gaya')
+    flow = Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE,
+        scopes=[
+            'https://www.googleapis.com/auth/photoslibrary.readonly',
+            'https://www.googleapis.com/auth/photoslibrary.appendonly',
+            'https://www.googleapis.com/auth/userinfo.profile',
+            'https://www.googleapis.com/auth/userinfo.email',
+            'openid'
+        ],
+        redirect_uri=redirect_uri
+    )
+    return flow
+
+def oauth(request):
+    print('oauth mai gaya')
+    return render(request, 'auth.html')
+
+def dest_oauth(request):
+    print('dest oauth mai gaya')
+    if not request.user.is_authenticated:
+        return redirect('oauth')
+    return render(request, 'destination_auth.html')
+
+def google_auth(request):
+    print('google auth mai gayay')
+    print(f'\n\nUser authenticated: {request.user.is_authenticated}')
+    flow = get_google_auth_flow('https://127.0.0.1:8000/photos/auth/callback/')
+    authorization_url, state = flow.authorization_url(prompt='select_account')
+    return redirect(authorization_url)
+
+def google_auth_callback(request):
+    print('google auth callback mai gaya')
+    if 'code' not in request.GET:
+        return redirect('home')
+
+    flow = get_google_auth_flow('https://127.0.0.1:8000/photos/auth/callback/')
+    flow.fetch_token(authorization_response=request.build_absolute_uri())
+    credentials = flow.credentials
+    print('creds in auth callback', credentials)
+
+    # Fetch user info using the token
+    userinfo_endpoint = 'https://www.googleapis.com/oauth2/v3/userinfo'
+    userinfo_response = requests.get(userinfo_endpoint, headers={
+        'Authorization': f'Bearer {credentials.token}'
+    })
+    
+    if userinfo_response.status_code == 200:
+        userinfo = userinfo_response.json()
+        print('userinfo', userinfo)
+        username = userinfo.get('name', 'Unknown User')
+        email = userinfo.get('email', 'Unknown Email')
+        print('username: ', username)
+        print('email: ', email)
+
+        # Save user info or use it
+        user, created = User.objects.get_or_create(username=email)
+        user.first_name = username
+        user.save()
+
+        login(request, user)
+        # if not request.session.get('is_destination_authenticated', False):
+        #     return redirect('destination_google_auth')
     else:
-        print('login first')
-    return redirect('photos_index')
+        print("Failed to fetch user info:", userinfo_response.text)
 
-def authenticate_destination(request):
-    if request.user.is_authenticated:
-        token = SocialToken.objects.filter(account__user=request.user, account__provider='google').last() 
-        print('dest_token', token.token)
-        if token and check_token_validity(token.token):
-            messages.success(request, 'Destination account authentication successful')
-        else:
-            messages.error(request, "Destination account auth failed")
-    print('login first')
-    return redirect('photos_index')
+    # Save credentials in session
+    request.session['source_credentials'] = credentials_to_dict(credentials)
+    return redirect('dest-oauth')
 
-def store_token_in_session(request, token, account_type):
-    key = f"{account_type}_token"
-    request.session[key] = token.token
 
-def get_token_from_session(request, account_type):
-    key = f"{account_type}_token"
-    return request.session.get(key, None)
+def credentials_to_dict(credentials):
+    print('credentials to dict mai gaya')
+    print('creds to dict function mai gaya')
+    return {
+        'token': credentials.token,
+        'refresh_token': credentials.refresh_token,
+        'token_uri': credentials.token_uri,
+        'client_id': credentials.client_id,
+        'client_secret': credentials.client_secret,
+        'scopes': credentials.scopes
+    }
 
+
+
+
+#################destination auth##########################
+
+def destination_google_auth(request):
+    print('destination google auth mai gaya')
+    # if request.method == 'POST':
+    #     email = request.POST.get('destination_email')
+    #     if not email:
+    #         print("Email is required for destination account authentication.")
+    #         return redirect('dest-oauth')  # Add error handling logic here
+
+        # **Save destination email for later use**
+
+    flow = get_google_auth_flow('https://127.0.0.1:8000/photos/destination/auth/callback/')
+    authorization_url, state = flow.authorization_url(access_type='offline', prompt='select_account')
+    return redirect(authorization_url)
+
+
+    
+
+def destination_google_auth_callback(request):
+    print('inside destination google auth callback')
+    if 'code' not in request.GET:
+        return redirect('dest-oauth')
+
+    flow = get_google_auth_flow('https://127.0.0.1:8000/photos/destination/auth/callback/')
+    flow.fetch_token(authorization_response=request.build_absolute_uri())
+    credentials = flow.credentials
+    print('creds in destination auth callback', credentials)
+    dest_creds = credentials_to_dict(credentials)
+    print('\n\ndest creds', dest_creds)
+    request.session['destination_credentials'] = dest_creds
+    request.session['is_destination_authenticated'] = True
+
+    # **Fetch and validate destination email**
+    destination_email = request.session.get('destination_email', None)
+
+    # **Fetch user info to confirm token validity**
+    userinfo = fetch_user_info(credentials)
+    print('userinfo', userinfo)
+    return redirect('migrate_photos')
+
+
+#####################logout#######################
 def logout_view(request):
+    source_credentials = request.session.get('source_credentials')
+    destination_credentials = request.session.get('destination_credentials')
+
+    if source_credentials:
+        requests.post(
+            'https://oauth2.googleapis.com/revoke',
+            params={'token': source_credentials['token']},
+            headers={'content-type': 'application/x-www-form-urlencoded'}
+        )
+
+    if destination_credentials:
+        requests.post(
+            'https://oauth2.googleapis.com/revoke',
+            params={'token': destination_credentials['token']},
+            headers={'content-type': 'application/x-www-form-urlencoded'}
+        )
+
+    request.session.flush()
+    logout(request)
+    return redirect('home')
+
+
+
+####################fetch user creds####################
+def fetch_user_info(credentials):
     try:
-        source_token = get_token_from_session(request, 'source')
-        if source_token:
-            blacklist_token(source_token)
-
-        destination_token = get_token_from_session(request, 'destination')
-        if destination_token:
-            blacklist_token(destination_token)
-
-        request.session.flush()
-        logout(request)
-        messages.success(request, "successfully logged out from source and destination")
-
-    except Exception as e:
-        print(f"error{e}")
-        messages.error(request, "An error occurred.")
-
-    return redirect('photos_index')
+        response = requests.get(
+            'https://www.googleapis.com/oauth2/v1/userinfo',
+            headers={'Authorization': f'Bearer {credentials.token}'}
+        )
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching user info: {e}")
+        return None
