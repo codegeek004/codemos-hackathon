d9ab54f47591827f7fa0439e850588918cbef897
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2025-01-17 15:33:10 +0530

    removed print statements added for debugging

--- django/photos/utils.py
+++ django/photos/utils.py
@@ -9,9 +9,7 @@ from django.contrib.auth.models import User
 
 
 def get_photos_service(credentials_dict):
-    print('inside get_photos_service')
     try:
-        print('in try of get_photos_service')
         #if credentials is already an instance of the object it will use existing 
         #object or else it will create another
         if isinstance(credentials_dict, Credentials):
@@ -29,60 +27,41 @@ def get_photos_service(credentials_dict):
             raise ValueError("credentials_dict must be either a Credentials object or a dictionary")
 
         if not credentials or not credentials.valid:
-            print("Invalid credentials")
             return None
 
         if credentials and credentials.expired and credentials.refresh_token:
             credentials.refresh(Request())
-        print('on top of service build')
-        service = build('photoslibrary', 'v1', credentials=credentials, static_discovery=False)
-        print('bottom of service build')
-        print('Service created:', service)
         return service
     except Exception as e:
-        print('inside exception')
-        print('Error in get_photos_service:', e)
         return None  
 
 
 def get_photos(credentials, page_token=None):
     try:
-        print('in try of get_photos')
         service = get_photos_service(credentials)
         results = service.mediaItems().list(pageSize=20, pageToken=page_token).execute()
-        print(results, 'result')
         items = results.get('mediaItems', [])
         next_page_token = results.get('nextPageToken')
-        print(f'\nitems \n{items}\n')
-        print(f"\nnext_page_tokens\n{next_page_token}\n")
         return items, next_page_token
     except Exception as e:
-        print('inside exception', e)
         return None
 
 
 def download_photo(url):
-    print('inside download photo')
-    print('url', url)
     if not User.is_authenticated:
         return redirect('oauth')
     try:
-        print('in try')
         response = requests.get(url, stream=True)
-        print('response ', response)
         response.raise_for_status()
         return io.BytesIO(response.content)
     except requests.exceptions.RequestException as e:
-        print('in except')
         print(f"Error downloading photo: {e}")
         return None
 
 def upload_photo(service, photo_data, file_name):
-    print('inside upload photo')
     if not User.is_authenticated:
         return redirect('oauth')
     try:
-        print('in try')
         upload_url = "https://photoslibrary.googleapis.com/v1/uploads"
         headers = {
             "Authorization": f"Bearer {service._http.credentials.token}",
@@ -90,18 +69,13 @@ def upload_photo(service, photo_data, file_name):
             "X-Goog-Upload-File-Name": file_name,
             "X-Goog-Upload-Protocol": "raw"
         }
-        print('on top of response')
 
         response = requests.post(upload_url, headers=headers, data=photo_data)
-        print('response', response)
         if response.status_code != 200:
-            print(f"Error uploading photos: {response.status_code}, {response.text}")
             return None
         response.raise_for_status()
 
         upload_token = response.text
-        print('upload tokens', upload_token)
-        print('photodata', photo_data)
         media_item = {
             'newMediaItems': [{
                 'simpleMediaItem': {
@@ -113,5 +87,4 @@ def upload_photo(service, photo_data, file_name):
         service.mediaItems().batchCreate(body=media_item).execute()
 
     except Exception as e:
-        print('in except')
-        print(f"Error uploading photo: {e}")
+        return e
