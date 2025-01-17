51021135898c864a384346507105205b7cfe9693
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2025-01-17 16:08:31 +0530

    removed unnecessary model

--- django/photos/models.py
+++ django/photos/models.py
@@ -1,7 +1,4 @@
 from django.db import models
 
 # Create your models here.
-class AuthenticationToken(models.Model):
-	source_token = models.CharField(max_length=100)
-	dest_token = models.CharField(max_length=100)
-	
+
