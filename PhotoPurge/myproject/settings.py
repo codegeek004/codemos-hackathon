cee4a6f2a27f02591cf746740f40180720a4f684
t <ajeymalviya143@gmail.com>
2025-01-20 13:08:13 +0530

    updated settings

--- PhotoPurge/myproject/settings.py
+++ PhotoPurge/myproject/settings.py
@@ -172,7 +172,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
