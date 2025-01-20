302bfcbbde6cd5cea2733f79165b98a751fe9880
ajaymalviy <ajeymalviya143@gmail.com>
2025-01-20 12:52:13 +0000

    updated settings file added light mode for allauthui

--- PhotoPurge/myproject/settings.py
+++ PhotoPurge/myproject/settings.py
@@ -171,8 +171,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL = 'static/'
+#STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
