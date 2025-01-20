a406c8623a40d2f520f37bc3678978ec0d3e84b1
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2025-01-20 14:38:44 +0530

    added custom user models to fetch the last_active time for the user

--- PhotoPurge/gmailapp/models.py
+++ PhotoPurge/gmailapp/models.py
@@ -1,19 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from django.utils.timezone import now
+
+class CustomUser(AbstractUser):
+    last_active = models.DateTimeField(default=now)
 
 class TaskStatus(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	task_id = models.CharField(max_length=200, unique=True)
-	status = models.CharField(max_length=40, default="PENDING")
-	result = models.TextField(null=True, blank=True)
-	deleted_count = models.IntegerField(default=0)
-	created_at = models.DateTimeField(auto_now_add=True)
-	
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE) 
+    task_id = models.CharField(max_length=200, unique=True)
+    status = models.CharField(max_length=40, default="PENDING")
+    result = models.TextField(null=True, blank=True)
+    deleted_count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 class RecoverStatus(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	task_id = models.CharField(max_length=200, unique=True)
-	status = models.CharField(max_length=40, default="PENDING")
-	result = models.TextField(null=True, blank=True)
-	recover_count = models.IntegerField(default=0)
-	created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)  
+    task_id = models.CharField(max_length=200, unique=True)
+    status = models.CharField(max_length=40, default="PENDING")
+    result = models.TextField(null=True, blank=True)
+    recover_count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
