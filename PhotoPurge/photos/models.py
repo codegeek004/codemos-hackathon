38a3cffefa5d3d54ce741ac3b9819c7050a42ff2
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2025-01-22 22:22:35 +0530

    added auth user model

--- PhotoPurge/photos/models.py
+++ PhotoPurge/photos/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
 class MigrationStatus(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE) 
+	user = models.ForeignKey('gmailapp.CustomUser', on_delete=models.CASCADE) 
 	task_id = models.CharField(max_length=200, unique=True)
 	status = models.CharField(max_length=40, default="PENDING")
 	result = models.TextField(null=True, blank=True)
