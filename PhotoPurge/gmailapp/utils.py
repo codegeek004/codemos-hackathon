170d2fc6052f5d9a35796c841bb18bae36a5ab42
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2025-01-24 18:20:20 +0530

    added strftime for fetching the data in format to insert in db

--- PhotoPurge/gmailapp/utils.py
+++ PhotoPurge/gmailapp/utils.py
@@ -1,14 +1,15 @@
 from google.oauth2.credentials import Credentials
 from allauth.socialaccount.models import SocialAccount, SocialToken
-
-def retrieve_credentials_for_user(user):
+from datetime import datetime
+def retrieve_credentials_for_user(user_id):
     try:
         # Get the social account for the user
-        social_account = SocialAccount.objects.get(user=user, provider="google")
+        social_account = SocialAccount.objects.get(user_id=user_id, provider="google")
         
         # Get the associated social token
         social_token = SocialToken.objects.get(account=social_account)
-        
+        time = social_token.expires_at
+        actual_time = time.strftime('%Y-%m-%d %H:%M:%S.%f')
         # Build the credentials
         from google.oauth2.credentials import Credentials
 
@@ -18,6 +19,7 @@ def retrieve_credentials_for_user(user):
             token_uri="https://oauth2.googleapis.com/token",
             client_id="your-client-id.apps.googleusercontent.com",
             client_secret="your-client-secret",
+            expiry=actual_time
         )
 
         return creds
