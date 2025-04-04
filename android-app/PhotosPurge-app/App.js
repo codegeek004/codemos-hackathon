12950ec1f1a5b7e6b3d66788faf7ab424089d96d
codegeek004 <yashvaishnav1404@gmail.com>
2025-04-04 23:33:00 +0530

    added welcome page adn removed the existing page just for testing

--- android-app/PhotosPurge-app/App.js
+++ android-app/PhotosPurge-app/App.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import GoogleLoginScreen from './GoogleLoginScreen';
-import FetchPhotosScreen from './FetchPhotosScreen';
+import { View, Text, StyleSheet } from 'react-native';
 
-const Stack = createStackNavigator();
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to PhotosPurge App 🎉</Text>
+    </View>
+  );
+}
 
-const App = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
-                <Stack.Screen name="Login" component={GoogleLoginScreen} />
-                <Stack.Screen name="FetchPhotos" component={FetchPhotosScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
-};
-
-export default App;
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
