868f0acee5616d479f9bc5bdebaa244bfe431467
codegeek004 <yashvaishnav1404@gmail.com>
2025-05-13 12:31:39 +0530

    added new readonly scopes which resolved the issue of some accounts not being able to access the media

--- PhotoPurge/codegeeks/settings.py
+++ PhotoPurge/codegeeks/settings.py
@@ -96,8 +96,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'gmail',
-        'USER': 'ajay',
-        'PASSWORD': 'Root@123',
+        'USER': 'root',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': 3306,
     }
@@ -107,15 +107,26 @@ DATABASES = {
 
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
-        'SCOPE': ['profile', 'email', 'https://mail.google.com/',  'https://www.googleapis.com/auth/photoslibrary'],
+        'SCOPE': [
+            'profile', 'email', 
+            'https://mail.google.com/',  
+            'https://www.googleapis.com/auth/photoslibrary',
+            'https://www.googleapis.com/auth/photoslibrary.sharing',
+            'https://www.googleapis.com/auth/drive.photos.readonly',
+            'https://www.googleapis.com/auth/photoslibrary.edit.appcreateddata',
+            'https://www.googleapis.com/auth/photoslibrary.appendonly',
+            'https://www.googleapis.com/auth/photoslibrary.readonly.appcreateddata',
+            
+
+            ],
         'AUTH_PARAMS': {
             'access_type': 'offline',
             'prompt' : 'consent' 
         },
         'OAUTH_PKCE_ENABLED': True,
         'APP': {
-              'client_id': '99034799467-hl9dbl4t4l64gftesd8bokb1no6kbgu3.apps.googleusercontent.com',
-              'secret': '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',
+              'client_id': '99034799467-m8dh7cdtpfquud1jvt21eup1t5vuk7fv.apps.googleusercontent.com',
+              'secret': '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',
             # for local development
             #'client_id': '99034799467-6l1lm0l6b80h2bcmon2i8st2odg778nj.apps.googleusercontent.com',
             #'secret': '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',
