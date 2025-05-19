72e2d63d5c86e0822e7170311ae3659f7358b888
codegeek004 <yashvaishnav1404@gmail.com>
2025-05-19 16:21:01 +0530

    added authentication screen

---
+++ android-app/PhotosPurgeApp/screens/AuthScreen.js
@@ -0,0 +1,45 @@
+import { NativeModules } from 'react-native';
+import { useState } from 'react';
+import { Button, View, Text, FlatList } from 'react-native';
+import TransferButton from '../components/TransferButton';
+
+const { PhotosAuth } = NativeModules;
+
+export default function AuthScreen() {
+  const [accounts, setAccounts] = useState([]);
+  const [tokenInfo, setTokenInfo] = useState(null);
+
+  const fetchAccounts = async () => {
+    const accs = await PhotosAuth.listGoogleAccounts();
+    setAccounts(accs);
+  };
+
+  const getTokenFor = async (accountName) => {
+    const tokenData = await PhotosAuth.getToken(accountName);
+    setTokenInfo(tokenData);
+  };
+
+  return (
+    <View style={{ padding: 20 }}>
+      <Button title="List Google Accounts" onPress={fetchAccounts} />
+      <FlatList
+        data={accounts}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => (
+          <Button title={item.name} onPress={() => getTokenFor(item.name)} />
+        )}
+      />
+
+      {tokenInfo && (
+        <View style={{ marginTop: 20 }}>
+          <Text>Selected Account: {tokenInfo.accountName}</Text>
+          <Text style={{ fontSize: 12 }}>Bearer Token: {tokenInfo.token}</Text>
+          <TransferButton
+            token={tokenInfo.token}
+            email={tokenInfo.accountName}
+          />
+        </View>
+      )}
+    </View>
+  );
+}
