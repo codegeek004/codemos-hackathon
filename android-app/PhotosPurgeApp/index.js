34aa7bbfe5ab8b43686441da5ff656bf71d03d6d
codegeek004 <yashvaishnav1404@gmail.com>
2025-05-19 16:16:04 +0530

    modified index file

--- android-app/PhotosPurgeApp/index.js
+++ android-app/PhotosPurgeApp/index.js
@@ -1,9 +1,8 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import App from './App'; // <- your main component
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+export default function MainApp() {
+  return <AuthScreen />;
+}
