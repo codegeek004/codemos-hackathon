fedc8e838ee2f4c2006a6ec6e54e303809eeafb6
codegeek004 <yashvaishnav1404@gmail.com>
2025-08-04 14:25:39 +0530

    resolved the issue of naive and aware time mismatch by creds.expired comparing with creds.expiry. I have changed the if creds.expired and creds.refresh_token with if creds.expired and creds.expiry<=now() and creds.refresh_token

--- PhotoPurge/gmailapp/middleware.py
+++ PhotoPurge/gmailapp/middleware.py
@@ -1,11 +1,12 @@
 from allauth.socialaccount.models import SocialAccount, SocialToken
 from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
-from django.utils.timezone import is_naive, make_aware
+from django.utils.timezone import is_naive, make_aware, now
 import logging
 from decouple import config
 from gmailapp.utils import ensure_aware
 from datetime import timezone
+
 
 logger = logging.getLogger(__name__)
 
@@ -25,12 +26,29 @@ class SourceTokenRefreshMiddleware:
                     token_uri="https://oauth2.googleapis.com/token",
                     client_id=config('client_id',cast=str),
                     client_secret=config('client_secret',cast=str),
-                    
-
+                    expiry=token.expires_at
                 )
-                creds.expiry = ensure_aware(token.expires_at)
+                # token.expires_at = ensure_aware(token.expires_at)
+                expiry = token.expires_at  # or token_obj.expiry for destination
+                print('creds k niche', creds.expiry)
+                if expiry:
+                    print('if creds k andar')
+                    if is_naive(expiry):
+                        print('if is_naive k andar')
+                        expiry = make_aware(expiry, timezone.utc)
+                        print('expiry hai', expiry)
+                    else:
+                        print('else k andar')
+                        expiry = expiry.astimezone(timezone.utc)
+                        print('idhar expiry', expiry)
+                creds.expiry = expiry
+                print('final creds', creds.expiry)
+                logger.debug(f"Source token expiry: {token.expires_at}, type: {type(token.expires_at)}")
+
+                # creds.expiry = token.expires_at
                 print(creds.expiry, 'source token expite', type(creds.expiry))
-                if creds.expiry and creds.refresh_token:
+                if creds.expiry and creds.expiry <= now() and creds.refresh_token:
+
                     creds.refresh(Request())
                     token.token = creds.token
                     token.expires_at = creds.expiry
