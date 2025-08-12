0cb3eb03bc823c02b37072312751a24c6b728d86
codegeek004 <yashvaishnav1404@gmail.com>
2025-08-12 22:33:58 +0530

    removed debugging statements and added raise ValueError and Exception in except condition

--- PhotoPurge/gmailapp/middleware.py
+++ PhotoPurge/gmailapp/middleware.py
@@ -24,40 +24,30 @@ class SourceTokenRefreshMiddleware:
                     token=token.token,
                     refresh_token=token.token_secret,
                     token_uri="https://oauth2.googleapis.com/token",
-                    client_id=config('client_id',cast=str),
-                    client_secret=config('client_secret',cast=str),
-                    expiry=token.expires_at
+                    client_id=config('client_id', cast=str),
+                    client_secret=config('client_secret', cast=str),
                 )
-                # token.expires_at = ensure_aware(token.expires_at)
-                expiry = token.expires_at  # or token_obj.expiry for destination
-                print('creds k niche', creds.expiry)
-                if expiry:
-                    print('if creds k andar')
-                    if is_naive(expiry):
-                        print('if is_naive k andar')
-                        expiry = make_aware(expiry, timezone.utc)
-                        print('expiry hai', expiry)
-                    else:
-                        print('else k andar')
-                        expiry = expiry.astimezone(timezone.utc)
-                        print('idhar expiry', expiry)
-                creds.expiry = expiry
-                print('final creds', creds.expiry)
-                logger.debug(f"Source token expiry: {token.expires_at}, type: {type(token.expires_at)}")
 
-                # creds.expiry = token.expires_at
-                print(creds.expiry, 'source token expite', type(creds.expiry))
+                expires_at = token.expires_at 
+
+                if expires_at:
+                    if is_naive(expires_at):
+                        expires_at = make_aware(expires_at, timezone.utc)
+                    else:
+                        expires_at = expires_at.astimezone(timezone.utc)
+
+                creds.expiry = expires_at
+
                 if creds.expiry and creds.expiry <= now() and creds.refresh_token:
 
                     creds.refresh(Request())
                     token.token = creds.token
                     token.expires_at = creds.expiry
                     token.save()
-                    logger.info(f"Refreshed source token for {request.user.email}")
 
             except (SocialAccount.DoesNotExist, SocialToken.DoesNotExist):
-                logger.debug(f"No source token found for {request.user}")
+                raise ValueError("Source token does not exist")
             except Exception as e:
-                logger.warning(f"Error refreshing source token: {e}")
+                raise Exception(f"Exception in SourceTokenRefreshMiddleware(gmailapp): {e}")
 
         return self.get_response(request)
