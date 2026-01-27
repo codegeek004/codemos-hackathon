87d8d2b2b0d238e3ebfa9f752a6a2405b30e973e
codegeek004 <yashvaishnav1404@gmail.com>
2026-01-27 15:26:23 +0530

    removed middleware token refresh middlewares

--- PhotoPurge/codegeeks/settings.py
+++ PhotoPurge/codegeeks/settings.py
@@ -64,8 +64,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
-    'gmailapp.middleware.SourceTokenRefreshMiddleware',
-    'photos.middleware.RefreshDestinationTokenMiddleware',
     
 ]
 
