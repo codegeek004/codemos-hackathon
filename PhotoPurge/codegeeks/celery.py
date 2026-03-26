c2302f98976dd658f3833104584fcbab7f98f344
codegeek004 <yashvaishnav1404@gmail.com>
2026-03-26 23:30:48 +0530

    replaced hardcoding with variables imported from .env

--- PhotoPurge/codegeeks/celery.py
+++ PhotoPurge/codegeeks/celery.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
-
+from decouple import config
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codegeeks.settings')
 
 app = Celery('codegeeks')
@@ -10,8 +10,8 @@ app = Celery('codegeeks')
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related config keys should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.conf.broker_url = 'redis://localhost:6379/0'  # Redis on localhost
-app.conf.result_backend = 'redis://localhost:6379/0'
+app.conf.broker_url = config('celery_broker_url', cast=str)
+app.conf.result_backend = config('celery_result_backend', cast=str)
 
 app.conf.update(result_expires=5400)
 
